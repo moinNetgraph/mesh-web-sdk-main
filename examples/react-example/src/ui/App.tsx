a4fb9042ce330700ded9c24087b823eb4fe5ce5c
moinNetgraph <moinuddin@netgraph.in>
2025-05-08 16:32:11 +0530

    Update App.tsx

--- examples/react-example/src/ui/App.tsx
+++ examples/react-example/src/ui/App.tsx
@@ -176,7 +176,7 @@ export const App: React.FC = () => {
         else {
          console.log('Summary', summary)
         setError(error || null)
-          const payload = 'Transaction cancelled!!'
+          const payload = '{error: "Transaction cancelled!!" }' 
           const form = document.createElement('form')
           form.method = 'POST'
           const hmacDigestSuccess = CryptoJS.HmacSHA256(payload, SECRET_KEY)
